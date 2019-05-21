769f4cbae71c675fcaceeccbb81ff59e604c6d62
Sean Doyle <sdoylelambda@gmail.com>
2019-05-21 16:51:57 -0400

    Get methods.

--- api/server.js
+++ api/server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const helmet = require('helmet');
 const knex = require('knex');
 
-const zooRouter = require('../zoo/zooRouter.js');
+const zoosRouter = require('../zoo/zooRouter.js');
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
-server.use('/api/zoo', zooRouter);
+server.use('/api/zoos', zoosRouter);
 
 module.exports = server;
--- zoo/zooRouter.js
+++ zoo/zooRouter.js
@@ -5,7 +5,7 @@ const config = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './data/rolex.db3'
+      filename: './data/lambda.sqlite3'
     },
     debug: true
   }
@@ -13,9 +13,18 @@ const config = {
   const db = knex(config);
 
   router.get('/', (req, res) => {
-      .this()
-      .catch()
-  })
+      db('zoos').then(records => res.status(201).json(records))
+      .catch(err => res.status(500).json({ error: "Cannot find records."}))
+  });
+
+  router.get('/:id', (req, res) => {
+        const { id } = req.params;
+        db('roles').where({ id })
+        .then(records => res.json(records))
+        .catch(err => res.status(500).json(err))
+  });
+
+  
 
   module.exports = router;
 
