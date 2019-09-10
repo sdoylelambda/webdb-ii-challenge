5a08194fa4af8654a181d4c80d146c6210c3ca6a
Sean Doyle <sdoylelambda@gmail.com>
2019-09-10 16:37:06 -0400

    Migrations created.

--- routers/zoo/zooRouter.js
+++ routers/zoo/zooRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const db = require('..data/db-config.js');
+
 router.get('/', (req, res) => {
     res.json({up:'its up'})
 });
