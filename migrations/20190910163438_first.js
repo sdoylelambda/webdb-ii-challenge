8965137badfb54f8a0133d20f6591ae85bfe053f
Sean Doyle <sdoylelambda@gmail.com>
2019-09-10 17:04:43 -0400

    Relational database added.

--- migrations/20190910163438_first.js
+++ migrations/20190910163438_first.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('zoos', tbl => {
+        tbl.increments();
+        tbl
+            .string('name', 128)
+            .notNullable()
+            .unique();
+    })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('zoos');
 };
--- routers/zoo/zooRouter.js
+++ routers/zoo/zooRouter.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
-const db = require('..data/db-config.js');
+// const db = require('..data/db-config.js');
+
+router.post('/', (req, res) => {
+    
+});
 
 router.get('/', (req, res) => {
     res.json({up:'its up'})
+
 });
+
+router.get('/', (req, res) => {
+    
+});
+
+router.delete('/', (req, res) => {
+    
+});
+
+router.put('/', (req, res) => {
+    
+});
+
+
 
 module.exports = router
