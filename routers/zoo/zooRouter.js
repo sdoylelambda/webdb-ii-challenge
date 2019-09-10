bd2220861928f9d9f8b9ce7ef8aaa75e4f99b33a
Sean Doyle <sdoylelambda@gmail.com>
2019-09-10 17:27:31 -0400

    Put CRUD method.

--- routers/zoo/zooRouter.js
+++ routers/zoo/zooRouter.js
@@ -3,18 +3,6 @@ const router = express.Router();
 
 const db = require('../../data/db-config');
 
-router.get('/', (req, res) => {
-    db('zoos')
-        .then(zoo => {
-            res.status(200).json(zoo);
-        })
-        .catch(err => res.status(400).json({ err:err }))
-});
-
-router.get('/', (req, res) => {
-    
-});
-
 router.post('/', (req, res) => {
     const newZoo = req.body
     db('zoos')
@@ -29,12 +17,54 @@ router.post('/', (req, res) => {
         .catch(err => res.status(500).json({ err:err }))
 });
 
-router.delete('/', (req, res) => {
-    
+router.get('/', (req, res) => {
+    db('zoos')
+        .then(zoo => {
+            res.status(200).json(zoo);
+        })
+        .catch(err => res.status(400).json({ err:err }))
 });
 
-router.put('/', (req, res) => {
-    
+router.get('/:id', (req, res) => {
+    const { id } = req.params.id
+    db('zoos')
+        .where({ id })
+        .first()
+        .then(fruit => {
+            res.status(200).json(fruit)
+        })
+        .catch(err => {
+            res.status(500).json({ err: err })
+        })
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params.id
+    db('zoos')
+        .where({ id })
+        .delete()
+        .then(del => {
+            res.status(200).json(del)
+        })
+        .catch(err => {
+            res.status(500).json({ err: err })
+        })
+});
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params.id
+    const newZoo = req.body
+    db('zoos')
+        
+        .then(ids => {
+            db('zoos')
+                .where({ id: ids[0] })
+                .insert(newZoo)
+                .then(newZooEntry => {
+                    res.status(200).json(newZooEntry)
+                })
+        })
+        .catch(err => res.status(500).json({ err:err }))
 });
 
 
