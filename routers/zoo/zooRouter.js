1e9e8649a83c91255a82c1a178b8f262e9b305de
Sean Doyle <sdoylelambda@gmail.com>
2019-09-10 17:14:27 -0400

    Get and post CRUD methods.

--- routers/zoo/zooRouter.js
+++ routers/zoo/zooRouter.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-// const db = require('..data/db-config.js');
+const db = require('../../data/db-config');
+
+router.get('/', (req, res) => {
+    db('zoos')
+        .then(zoo => {
+            res.status(200).json(zoo);
+        })
+        .catch(err => res.status(400).json({ err:err }))
+});
+
+router.get('/', (req, res) => {
+    
+});
 
 router.post('/', (req, res) => {
-    
-});
-
-router.get('/', (req, res) => {
-    res.json({up:'its up'})
-
-});
-
-router.get('/', (req, res) => {
-    
+    const newZoo = req.body
+    db('zoos')
+        .insert(newZoo)
+        .then(ids => {
+            db('zoos')
+                .where({ id: ids[0] })
+                .then(newZooEntry => {
+                    res.status(200).json(newZooEntry)
+                })
+        })
+        .catch(err => res.status(500).json({ err:err }))
 });
 
 router.delete('/', (req, res) => {
